222291fb287ead0b78f3d70c84c109b4b44e257e
AZ7007 <azsali2404@gmail.com>
2025-05-12 00:03:15 +0100

    set up structure of game function

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function funGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+ function calculateCorrectAnswer(){
+
+ }
+
+ 
+
+ function incrementScore() {
+
+ }
+
+
+
+  function incrementWrongAnswer() {
+    
+ }
+
+
+
+ function displayAdditionQuestion() {
+
+ }
+
+
+
+ function displaySubtractQuestion() {
+
+ }
+
+
+
+ function displayMultiplyQuestion() {
+
+ }
