eb8f2a61f4ccfe20132efa3e7df1121d3f34ecbe
AZ7007 <azsali2404@gmail.com>
2025-05-12 02:23:59 +0100

    diplay addtion question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,10 +8,13 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("you clicked Submit");
         } else {
             let gameType = this.getAttribute("data-type")
-            alert(`you clicked ${gameType}`);
+            runGame(gameType);
         }
     })
   }
+
+  runGame("addition");
+
 })
 
 
@@ -19,10 +22,18 @@ document.addEventListener("DOMContentLoaded", function() {
  * The main game "loop", called when the script is first loading
  * and after the users answer has been processed 
  */
-function funGame() {
+function runGame(gameType) {
 
 let num1 = Math.floor(Math.random() * 25) + 1;// random number between 1 and 25
 let num2 = Math.floor(Math.random() * 25) + 1; 
+
+if(gameType === "addition") {
+displayAdditionQuestion(num1, num2);
+
+} else {
+    alert(`Unknown game type: ${gameType}`);
+    throw `Unknown game type: ${gameType}. Aborting`;
+}
 
 } 
 
@@ -50,8 +61,11 @@ function checkAnswer() {
 
 
 
- function displayAdditionQuestion() {
-
+ function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1
+    document.getElementById("operand2").textContent = operand2
+    document.getElementById("operator").textContent = "+"
+      
  }
 
 
